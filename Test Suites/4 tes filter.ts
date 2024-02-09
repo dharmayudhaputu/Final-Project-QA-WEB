af9a9754001d8d6741b9afcfd7c88d2b7859f304
dharmayudhaputu <dharmayudhaputu@gmail.com>
2024-02-09 23:18:50 +0800

    commit 21 - update file excel dan script data driven pada test case

--- Test Suites/4 tes filter.ts
+++ Test Suites/4 tes filter.ts
@@ -15,7 +15,7 @@
       <guid>81192e83-1edc-47c7-888b-05ec0af645c5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1. Positive test case/5 Filter item (switch-case)</testCaseId>
+      <testCaseId>Test Cases/1. Positive test case/5 Filter item (switch-case and data driven)</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
          <id>c171bc87-c5ae-4ee7-b2c5-b33677733d00</id>
@@ -31,6 +31,28 @@
          <type>DATA_COLUMN</type>
          <value>filtername</value>
          <variableId>cb2a85f2-e5b8-48b3-9189-d0f9225544bd</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99a52eb0-9a54-4bbc-a8a8-9f9ac09c8b1f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1. Positive test case/6 Add to Chart (switch-case and data driven)</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1d70ea54-a326-427f-af5a-10e28e32b581</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/4 data laptop</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>1d70ea54-a326-427f-af5a-10e28e32b581</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>datalaptop</value>
+         <variableId>9b65570e-f4fb-41b6-9911-ab69f889e43c</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
