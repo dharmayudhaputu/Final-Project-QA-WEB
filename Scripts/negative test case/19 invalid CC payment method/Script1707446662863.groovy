637e08e095b92ba10797ec41cfa3965cd23d90fd
dharmayudhaputu <dharmayudhaputu@gmail.com>
2024-02-09 14:49:15 +0800

    commit 13 - test case gagal bayar dengan CC

--- Scripts/negative test case/19 invalid CC payment method/Script1707446662863.groovy
+++ Scripts/negative test case/19 invalid CC payment method/Script1707446662863.groovy
@@ -17,3 +17,48 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://advantageonlineshopping.com/')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/path_CHECKOUT  (0.00)_menuUserSVGPath'))
+
+WebUI.setText(findTestObject('Object Repository/invalid CC Payment Method/input_OR_username'), 'yudhatesting')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/invalid CC Payment Method/input_Username_password'), 'VjyC/is+sZNhzZxZ8kSNJw==')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/button_SIGN IN'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/div_TABLETS                        Shop Now'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/img_SHOP NOW_16'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/button_ADD TO CART'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/a_2'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/button_CHECKOUT (1,278.99)'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/button_NEXT'))
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/img'))
+
+WebUI.setText(findTestObject('Object Repository/invalid CC Payment Method/input__card_number'), '1234 5678 9002')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/label_Cardholder name'))
+
+WebUI.setText(findTestObject('Object Repository/invalid CC Payment Method/input__cardholder_name'), 'dharmayudhaputu')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/label_CVV number'))
+
+WebUI.setText(findTestObject('Object Repository/invalid CC Payment Method/input__cvv_number'), '2')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/div_1. SHIPPING DETAILS         2. PAYMENT _93ea38'))
+
+WebUI.setText(findTestObject('Object Repository/invalid CC Payment Method/input__card_number'), '1234 5678 900')
+
+WebUI.click(findTestObject('Object Repository/invalid CC Payment Method/div_1. SHIPPING DETAILS         2. PAYMENT _93ea38_1'))
+WebUI.delay(5)
+
+WebUI.closeBrowser()
+
